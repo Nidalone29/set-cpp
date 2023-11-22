55ca3b87e5cf1f28547c28c81d57af9885042b75
Alessandro Burza <thelorizz@gmail.com>
2023-11-22 12:45:47 +0000

    Ciao Nidal@

---
+++ src/ciaonidal.cpp
@@ -0,0 +1,12 @@
+#include "ciaonidal.h"
+
+void CiaoNidal::Hello() {
+    std::cout << "Ciao Nidal" << std::endl;
+
+    std::string answer;
+    std::getline(std::cin, answer);
+
+    if (!answer.empty()) {
+        std::cout << "XDLOL" << std::endl;
+    }
+}
---
+++ src/ciaonidal.h
@@ -0,0 +1,12 @@
+#ifndef CIAONIDAL_H
+#define CIAONIDAL_H
+
+#include <iostream>
+#include <string>
+
+class CiaoNidal {
+public:
+    void Hello();
+};
+
+#endif
